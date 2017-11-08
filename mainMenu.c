063e242e3036af12e0ca8972b02feb53ee5fd1df
guwenjun <guwenjun2017@gmail.com>
2017-11-09 00:15:48 +0800

    aaa

---
+++ mainMenu.c
@@ -0,0 +1,35 @@
+#include "stu.h"
+  
+/*********************************************主模块****************************************************/
+int mainMenu()
+{
+    int choice;
+    IO_ReadInfo();
+    do{
+	//system("sl");
+	system("sh do.sh");
+	system("clear");
+	printf("\e[32m\e[1m%s\e[0m","|---------------------------------------------|\n");
+	printf("\e[32m\e[1m%s\e[0m","|                 main                        |\n");
+	printf("\e[32m\e[1m%s\e[0m","|---------------------------------------------|\n");
+	printf("\e[32m\e[1m%s\e[0m","|      1 ---- Result input                    |\n");
+	printf("\e[32m\e[1m%s\e[0m","|      2 ---- Achievement statistics          |\n");
+	printf("\e[32m\e[1m%s\e[0m","|      3 ---- Student status management       |\n");
+	printf("\e[32m\e[1m%s\e[0m","|      0 ---- Exit                            |\n");
+	printf("\e[32m\e[1m%s\e[0m","|---------------------------------------------|\n");
+	printf("\e[32m\e[1m%s\e[0m","Please choose(1 ~ 3, 0 exit):\n");
+	scanf("%d", &choice);
+	switch(choice)
+	{
+	    case 1: GradeInput(); break;
+	    case 2: GradeTongJi(); break;
+	    case 3: Student_status_management(); break;
+	    case 0: exit(0);
+	    default: printf("\e[31m\e[1m%s\e[0m","It is an illegal option, and press any key to choose again!\n");
+	    getchar();
+	    getchar();
+	}
+    }while(1);
+
+    return 0;
+}
--- test.c
+++ test.c
@@ -1,37 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+/* *========* *** *============================================== 
+  * 联络信箱: *** :guwenjun2017@gmail.com
+  * 文档用途: *** :CII实验报告
+  * 文档信息: *** :~/workspace/StudentsManagementSystem/test.c
+  * 修订时间: *** *2017年第45周 11月09日 星期四 上午12:15 (313天)
+ * *========* *** *============================================== */
 #include "stu.h"
-  
-/*********************************************主模块****************************************************/
 int main(int argc, char **argv)
 {
-    int choice;
-    IO_ReadInfo();
-    do{
-	//system("sl");
-	system("sh do.sh");
-	system("clear");
-	printf("\e[32m\e[1m%s\e[0m","|---------------------------------------------|\n");
-	printf("\e[32m\e[1m%s\e[0m","|                 main                        |\n");
-	printf("\e[32m\e[1m%s\e[0m","|---------------------------------------------|\n");
-	printf("\e[32m\e[1m%s\e[0m","|      1 ---- Result input                    |\n");
-	printf("\e[32m\e[1m%s\e[0m","|      2 ---- Achievement statistics          |\n");
-	printf("\e[32m\e[1m%s\e[0m","|      3 ---- Student status management       |\n");
-	printf("\e[32m\e[1m%s\e[0m","|      0 ---- Exit                            |\n");
-	printf("\e[32m\e[1m%s\e[0m","|---------------------------------------------|\n");
-	printf("\e[32m\e[1m%s\e[0m","Please choose(1 ~ 3, 0 exit):\n");
-	scanf("%d", &choice);
-	switch(choice)
-	{
-	    case 1: GradeInput(); break;
-	    case 2: GradeTongJi(); break;
-	    case 3: Student_status_management(); break;
-	    case 0: exit(0);
-	    default: printf("\e[31m\e[1m%s\e[0m","It is an illegal option, and press any key to choose again!\n");
-	    getchar();
-	    getchar();
-	}
-    }while(1);
+    mainMenu();
 
     return 0;
 }
